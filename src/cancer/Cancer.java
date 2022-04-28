64c495c38a1686292385a4550be7cb4602eb23a7
BrycenPiccolotto <brycenpiccolotto@gmail.com>
2022-04-28 14:02:49 -0400

    3rd commit

--- src/cancer/Cancer.java
+++ src/cancer/Cancer.java
@@ -51,7 +51,6 @@ public class Cancer {
             }
         }
         
-        
         for(int i = 0; i<grid.length;i++){
             for(int j = 0; j<grid[0].length;j++){
                 System.out.print(grid[i][j]);
